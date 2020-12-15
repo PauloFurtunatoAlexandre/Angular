554429c53818eff52d1dba9a63a74dc44a02ba70
Paulo Alexandre <furtuna.alexandre@gmail.com>
2020-12-15 10:12:16 -0600

    added routes with path variable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { StarComponent } from './courses/star-component';
 import { ReplacePipe } from './courses/pipe/replace.pipe';
 import { NavbarComponent } from './courses/navbar/navbar.component';
 import { Error404Component } from './error404/error404.component';
+import { CourseInfoComponent } from './courses/course-info.component';
 
 @NgModule({
   declarations: [
@@ -18,19 +19,24 @@ import { Error404Component } from './error404/error404.component';
     ReplacePipe,
     NavbarComponent,
     Error404Component,
+    CourseInfoComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
       {
+        path: 'courses',
+        component: CourseListComponent,
+      },
+      {
+        path: 'courses/info/:id',
+        component: CourseInfoComponent,
+      },
+      {
         path: '',
         redirectTo: 'courses',
         pathMatch: 'full',
-      },
-      {
-        path: 'courses',
-        component: CourseListComponent,
       },
       {
         path: '**',
